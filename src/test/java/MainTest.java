13a4aaea5b75ab85f78720bd8a3422790dc5b7b7
Tatiana <tatshi2104@gmail.com>
2024-09-13 10:22:34 -0500

    manual test sctipt

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -6,5 +6,78 @@ class MainTest {
 
     @Test
     void main() {
+        /*
+  Credentials
+  Gmail.
+John Doe
+January 1, 1975
+doe11247@gmail.com
+password: Yekaterinburg312
+Recovery mobile number: (346) 701-9585
+
+Project link.
+https://pearson.jobs/jobs/?q=Talent+Acquisition+Specialist
+
+
+
+        Test 1
+
+1.Open link https://pearson.jobs/jobs/?q=Talent+Acquisition+Specialist in browser Google chrome
+Website is downloaded.
+
+2.Go to "Join Our Talent Community" button and click it.
+ Empty form for the candidate will appear.
+
+4. Go to e-mail address field and input e-mail address.
+   The e-mail address is populated.
+5.Click "I agree with terms and conditions" box.
+The box is ticked.
+
+6. On right side at the bottom click subscribe button.
+ The candidate is subscribed to the mail list.
+
+7. Go to Gmail website:
+https://accounts.google.com/InteractiveLogin/signinchooser?continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&osid=1&passive=1209600&service=mail&ifkv=Ab5oB3rrPIFLo1LBbodDcdSSKiHxCIk0OB4m0IJKjGKynTpOIwlB8PbZ1GpW7G6m1-20VIieXa7IVA&ddm=0&flowName=GlifWebSignIn&flowEntry=ServiceLogin
+
+The website is downloaded
+
+8. Choose https://accounts.google.com/InteractiveLogin/signinchooser?continue=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&emr=1&followup=https%3A%2F%2Fmail.google.com%2Fmail%2Fu%2F0%2F&osid=1&passive=1209600&service=mail&ifkv=Ab5oB3rrPIFLo1LBbodDcdSSKiHxCIk0OB4m0IJKjGKynTpOIwlB8PbZ1GpW7G6m1-20VIieXa7IVA&ddm=0&flowName=GlifWebSignIn&flowEntry=ServiceLogin
+ John Doe account is chosen from the list.
+
+9. Click the chosen account.
+   Empty password field appeared.
+
+10. Fill out the password
+    The password is populated.
+
+11. Press next button.
+    Account page appeared.
+
+12. Check that sign in conformation letter appeared on the page.
+    the conformation letter is in inbox folder.
+
+13. Click J icon on the top right corner of the screen.
+    The pop-up screen appeared.
+
+14.Choose sign out and click it.
+   The client is signed out.
+
+Test 2
+
+1. Go to homepage
+   Homepage is downloaded
+2. Scroll down to remote jobs and pick Executive Sales Specialist by clicking an empty box opposite to the role.
+   the description of the position appears on the screen
+
+2. Click apply now button
+E-mail field appears on the screen.
+
+3. Populate E-mail address field and click I agree with terms and conditions field and click next button.
+  The questionnaire appears on the screen.
+
+4. Filled out the questionnaire.
+
+
+         */
     }
 }
